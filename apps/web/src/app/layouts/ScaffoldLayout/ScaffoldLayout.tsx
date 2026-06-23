898a998d90f0092be3e5ae8ca1c710e2253d19ed
Simon Gagnon <git@simoncrypta.dev>
2026-06-23 14:52:26 -0400

    Add blog and scaffold layouts.

---
+++ apps/web/src/app/layouts/ScaffoldLayout/ScaffoldLayout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Link, routes } from "@/app/redwood/router";
+import { Toaster } from "@/app/redwood/toast";
+
+type LayoutProps = {
+  title: string;
+  titleTo: "contacts" | "posts";
+  buttonLabel: string;
+  buttonTo: "newContact" | "newPost";
+  children: React.ReactNode;
+};
+
+const ScaffoldLayout = ({ title, titleTo, buttonLabel, buttonTo, children }: LayoutProps) => {
+  return (
+    <div className="rw-scaffold">
+      <Toaster toastOptions={{ className: "rw-toast", duration: 6000 }} />
+      <header className="rw-header">
+        <h1 className="rw-heading rw-heading-primary">
+          <Link to={routes[titleTo]()} className="rw-link">
+            {title}
+          </Link>
+        </h1>
+        <Link to={routes[buttonTo]()} className="rw-button rw-button-green">
+          <div className="rw-button-icon">+</div> {buttonLabel}
+        </Link>
+      </header>
+      <main className="rw-main">{children}</main>
+    </div>
+  );
+};
+
+export default ScaffoldLayout;
